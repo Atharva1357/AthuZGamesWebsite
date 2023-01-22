36fe2f30492e397999569d1be53cf669c4f4f6d0
Atharva1357 <92341172+Atharva1357@users.noreply.github.com>
2023-01-22 08:37:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+document.getElementById("button2").style.visibility = "hidden"
+var color = document.getElementById("button1");
+function darkMode()
+{
+        document.getElementById("button1").style.backgroundColor = "#808080";
+        document.getElementById("html").style.backgroundColor = "#808080";
+        document.getElementById("button1").style.visibility = "hidden"
+        document.getElementById("button2").style.visibility = "visible"
+        if (color.style.backgroundColor == "#ffffff")
+        {
+            console.log("detection_color")
+        }
+        console.log("Darkmode On")
+}
+function lightMode()
+{
+    document.getElementById("button1").style.backgroundColor = "#ffffff";
+        document.getElementById("html").style.backgroundColor = "#ffffff";
+        document.getElementById("button1").style.visibility = "visible"
+        document.getElementById("button2").style.visibility = "hidden"
+        console.log("Darkmode Off")
+}
